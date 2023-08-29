7151ff21bc9ccf3b5f5abe9ac9ccf7ba3cda4580
softsmyth <geekgineering@gmail.com>
2023-08-29 16:55:48 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,19 +4,28 @@ namespace b4k{
     */
     //% blockId=leftdcmotor
     //% block="Left DC Motor $ldir, $lpwr"
-    //% ldir.min=0 ldir.max=1
+    //% ldir.min=0 ldir.max=2
     //% lpwr.min=0 lpwr.max=1023
     export function leftDCmotor(ldir: number,lpwr: number){
         pins.digitalWritePin(DigitalPin.P13, ldir);
-        pins.analogWritePin(AnalogPin.P12, lpwr);
+        if (ldir > 0) {
+            pins.analogWritePin(AnalogPin.P12, lpwr);
+        } else {
+            pins.digitalWritePin(DigitalPin.P12, 0)
+        }
     }
     //% blockId=rightDCmotor
     //% block="Right DC Motor $rdir, $rpwr"
-    //% rdir.min=0 rdir.max=1
+    //% rdir.min=0 rdir.max=2
     //% rpwr.min=0 rpwr.max=1023
     export function rightDCmotor(rdir: number, rpwr: number) {
         pins.digitalWritePin(DigitalPin.P14, rdir);
-        pins.analogWritePin(AnalogPin.P15, rpwr);
+        if(rdir>0){
+            pins.analogWritePin(AnalogPin.P15, rpwr);
+        }else{
+            pins.digitalWritePin(DigitalPin.P15, 0)
+        }
+        
     }
     export function leftServo(angle: number) {
         
