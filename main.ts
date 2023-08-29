ff614cbc7fcf234341ac034a82ee0ccf8a912979
softsmyth <geekgineering@gmail.com>
2023-08-29 16:26:00 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,14 +2,16 @@ namespace b4k{
     /*
     *   b4k handler
     */
-    //% blockId="Left DC Motor $ldir, $lpwr"
+    //% blockId=leftdcmotor
+    //% block="Left DC Motor $ldir, $lpwr"
     //% ldir.min=0 ldir.max=1
     //% lpwr.min=0 lpwr.max=1023
     export function leftDCmotor(ldir: number,lpwr: number){
         pins.digitalWritePin(DigitalPin.P13, ldir);
         pins.analogWritePin(AnalogPin.P12, lpwr);
     }
-    //% blockId="Right DC Motor $rdir, $rpwr"
+    //% blockId=rightDCmotor
+    //% block="Right DC Motor $rdir, $rpwr"
     //% rdir.min=0 rdir.max=1
     //% rpwr.min=0 rpwr.max=1023
     export function rightDCmotor(rdir: number, rpwr: number) {
