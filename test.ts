6016bb1ef1655fa208bb3892d4a92b261d986bba
softsmyth <geekgineering@gmail.com>
2023-08-30 11:40:27 -0400

    Added tests

--- test.ts
+++ test.ts
@@ -1,4 +1,14 @@
 // tests go here; this will not be compiled when this package is used as an extension.
 b4k.motorA(0,1023);
 b4k.motorB(0,1023);
-b4k.
+b4k.servo1(0);
+basic.pause(1000);
+b4k.servo1(180);
+basic.pause(1000);
+b4k.servo1(360);
+basic.pause(1000);
+b4k.servo2(0);
+basic.pause(1000);
+b4k.servo2(180);
+basic.pause(1000);
+b4k.servo2(360);
