7ab5539648b7579fef3abcfeceba60d92ac51482
softsmyth <geekgineering@gmail.com>
2023-09-09 15:04:45 -0400

    block changes

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 namespace b4k {
     /** b4k handler */
-    //  % blockId=dcmotora
-    //  % block="DC Motor A $adir, $apwr"
-    //  % adir.min=0 adir.max=2
-    //  % apwr.min=0 apwr.max=1023
+    //% blockId=dcmotora
+    //% block="DC Motor A $adir, $apwr"
+    //% adir.min=0 adir.max=2
+    //% apwr.min=0 apwr.max=1023
     export function motorA(adir: number, apwr: number) {
         pins.digitalWritePin(DigitalPin.P13, adir)
         if (adir < 2) {
@@ -14,10 +14,10 @@ namespace b4k {
         
     }
     
-    //  % blockId=dcmotorb
-    //  % block="DC Motor B $bdir, $bpwr"
-    //  % bdir.min=0 bdir.max=2
-    //  % bpwr.min=0 bpwr.max=1023
+    //% blockId=dcmotorb
+    //% block="DC Motor B $bdir, $bpwr"
+    //% bdir.min=0 bdir.max=2
+    //% bpwr.min=0 bpwr.max=1023
     export function motorB(bdir: number, bpwr: number) {
         pins.digitalWritePin(DigitalPin.P14, bdir)
         if (bdir < 2) {
@@ -28,16 +28,16 @@ namespace b4k {
         
     }
     
-    //  % blockId=servoOne
-    //  % block="Servo 1 $ang1"
-    //  % ang1.min=0 ang1.max=360
+    //% blockId=servoOne
+    //% block="Servo 1 $ang1"
+    //% ang1.min=0 ang1.max=360
     export function servo1(ang1: number) {
         pins.servoWritePin(AnalogPin.P1, ang1 / 2)
     }
     
-    //  % blockId=servoTwo
-    //  % block="Servo 2 $ang2"
-    //  % ang2.min=0 ang2.max=360
+    //% blockId=servoTwo
+    //% block="Servo 2 $ang2"
+    //% ang2.min=0 ang2.max=360
     export function servo2(ang2: number) {
         pins.servoWritePin(AnalogPin.P2, ang2 / 2)
     }
