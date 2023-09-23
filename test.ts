6de8daa15f8b1520dd6a8b8225670f52f5ea831f
softsmyth <geekgineering@gmail.com>
2023-09-23 17:12:29 -0400

    Fix range for DC motors in test.ts

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 // tests go here; this will not be compiled when this package is used as an extension.
-Bricks4Kidz.motorA(0,1023);
-Bricks4Kidz.motorB(0,1023);
+Bricks4Kidz.motorA(0,100);
+Bricks4Kidz.motorB(0,100);
 Bricks4Kidz.servo1(0);
 basic.pause(1000);
 Bricks4Kidz.servo1(180);
