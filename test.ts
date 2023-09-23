4410a7a26b956ff75d04375f70e57db11a9f1e71
softsmyth <geekgineering@gmail.com>
2023-09-23 15:59:25 -0400

    Updated test.ts to new namespace name

--- test.ts
+++ test.ts
@@ -1,14 +1,14 @@
 // tests go here; this will not be compiled when this package is used as an extension.
-b4k.motorA(0,1023);
-b4k.motorB(0,1023);
-b4k.servo1(0);
+Bricks4Kidz.motorA(0,1023);
+Bricks4Kidz.motorB(0,1023);
+Bricks4Kidz.servo1(0);
 basic.pause(1000);
-b4k.servo1(180);
+Bricks4Kidz.servo1(180);
 basic.pause(1000);
-b4k.servo1(360);
+Bricks4Kidz.servo1(360);
 basic.pause(1000);
-b4k.servo2(0);
+Bricks4Kidz.servo2(0);
 basic.pause(1000);
-b4k.servo2(180);
+Bricks4Kidz.servo2(180);
 basic.pause(1000);
-b4k.servo2(360);
+Bricks4Kidz.servo2(360);
